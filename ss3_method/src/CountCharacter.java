16faaaf0503049e77a327fcb57f731252f6b874f
DaiHoang0910 <hoangvandai0910@gmail.com>
2024-10-16 09:28:20 +0700

    bai tap

--- ss3_method/src/CountCharacter.java
+++ ss3_method/src/CountCharacter.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CountCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Enter a string: ");
+        str = sc.nextLine();
+
+        char ch;
+        System.out.println("Enter a character: ");
+        ch = sc.next().charAt(0);
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (ch == str.charAt(i)) {
+                count++;
+            }
+        }
+        System.out.println("The number of occurrences of a character" + ch + " in a string: " + count);
+    }
+
 }
