f370e57a0cc00a5d19775c722a8274fbab1badaf
DaiHoang0910 <hoangvandai0910@gmail.com>
2024-10-16 16:25:15 +0700

    bai tap

---
+++ ss6_Inheritance/src/Circle/Test.java
@@ -0,0 +1,15 @@
+package Circle;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(5);
+        circle.setColor("red");
+        System.out.println("Area of circle: " + circle.getArea());
+        System.out.println("Color of circle: " + circle.getColor());
+
+        Cylinder cylinder = new Cylinder(circle.getRadius(), circle.getColor(), 10 );
+        System.out.println("Total of cylinder: " + cylinder.getTotalArea());
+
+    }
+}
