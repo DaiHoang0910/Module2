5568adb976f689e52bc418ee5c00baeb73a63a99
DaiHoang0910 <hoangvandai0910@gmail.com>
2024-10-17 08:30:44 +0700

    bai tap

---
+++ ss6_Inheritance/src/Point2D_Point3D/Test.java
@@ -0,0 +1,10 @@
+package Point2D_Point3D;
+
+public class Test {
+    public static void main(String[] args) {
+        Point2D point2D_1 = new Point2D(3, 4);
+        Point3D point3D_1 = new Point3D(4, 5, 7);
+        System.out.println(point2D_1.toString());
+        System.out.println(point3D_1.toString());
+    }
+}
