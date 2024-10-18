ec099c4757a76fdb9497f5ac306df579bf123a63
DaiHoang0910 <hoangvandai0910@gmail.com>
2024-10-18 09:24:35 +0700

    bai tap

--- ss7_interface/src/bai_tap/bai1/Resizeable.java
+++ ss7_interface/src/bai_tap/bai1/Resizeable.java
@@ -1,5 +1,5 @@
 package bai_tap.bai1;
 
 public interface Resizeable {
-    public abstract void resize(double percent);
+    void resize(double percent);
 }
--- ss7_interface/src/bai_tap/bai2/Colorable.java
+++ ss7_interface/src/bai_tap/bai2/Colorable.java
@@ -1,5 +1,5 @@
 package bai_tap.bai2;
 
 public interface Colorable {
-    public abstract String howToColor();
+    String howToColor();
 }
